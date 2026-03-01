63f634a52f39e32f2b313b83bb0995d415502b18
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2026-03-01 16:19:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler
+from config import API_TOKEN
+from handlers.start_handler import start
 
+def main():
+    # Initialize the Updater and Dispatcher
+    updater = Updater(API_TOKEN)
+    dispatcher = updater.dispatcher
+
+    # Register the start command handler
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    # Start the bot
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
