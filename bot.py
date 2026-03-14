4bc7f1ea372a75fede0b50e99819232bbe60fd58
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2026-03-14 21:15:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,23 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from config import BOT_TOKEN  # <-- config se token le rahe hain
 
+# /start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "✅ Bot Online\n\nCommands:\n/start\n/help"
     )
 
+# /help command
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "⚡ Commands:\n/start\n/help"
     )
 
+# Build app
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
+# Add handlers
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("help", help))
 
